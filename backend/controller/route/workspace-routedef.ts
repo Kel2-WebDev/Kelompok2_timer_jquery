913ea592032997e6e97e08aa11438d40c73c43af
Fauzan Lubis <fauzanfauh45@gmail.com>
2021-10-07 20:21:01 +0700

    Refactor workspace API, Create a comon routedef

---
+++ backend/controller/route/workspace-routedef.ts
@@ -0,0 +1,50 @@
+import { Type, Static } from "@sinclair/typebox";
+import { ErrorMessage } from "../common-routedef";
+
+/**
+ * Prefix /workspace
+ */
+
+export const Workspace = Type.Object({
+  id: Type.String(),
+  title: Type.String(),
+  description: Type.String(),
+  lastUsed: Type.Number(),
+});
+export type Workspace = Static<typeof Workspace>;
+
+/* GET /:id */
+export const GetWorkspaceParams = Type.Object({
+  id: Type.String(),
+});
+export type GetWorkspaceParams = Static<typeof GetWorkspaceParams>;
+
+export const GetWorkspaceResponse = Workspace;
+export type GetWorkspaceResponse = Static<typeof GetWorkspaceResponse>;
+
+export const GetWorkspaceError = ErrorMessage;
+export type GetWorkspaceError = ErrorMessage;
+
+/* POST / */
+export const NewWorkspaceBody = Type.Omit(Workspace, ["id", "lastUsed"]);
+export type NewWorkspaceBody = Static<typeof NewWorkspaceBody>;
+
+export const NewWorkspaceResponse = Workspace;
+export type NewWorkspaceResponse = Workspace;
+
+/* PUT /:id */
+export const UpdateWorkspaceParams = Type.Object({
+  id: Type.String(),
+});
+export type UpdateWorkspaceParams = Static<typeof UpdateWorkspaceParams>;
+
+export const UpdateWorkspaceBody = Type.Optional(
+  Type.Omit(Workspace, ["id", "lastUsed"])
+);
+export type UpdateWorkspaceBody = Static<typeof UpdateWorkspaceBody>;
+
+export const UpdateWorkspaceResponse = Workspace;
+export type UpdateWorkspaceResponse = Workspace;
+
+export const UpdateWorkspaceError = ErrorMessage;
+export type UpdateWorkspaceError = ErrorMessage;
--- backend/controller/route/workspace.ts
+++ backend/controller/route/workspace.ts
@@ -1,25 +1,47 @@
-import { Static, Type } from "@sinclair/typebox";
 import { FastifyPluginAsync } from "fastify";
 import humanId from "human-id";
-
-export const Workspace = Type.Object({
-  id: Type.String(),
-  title: Type.String(),
-  description: Type.String(),
-  lastUsed: Type.Number(),
-});
-export type Workspace = Static<typeof Workspace>;
-
-export const NewWorkspaceBody = Type.Omit(Workspace, ["id", "lastUsed"]);
-export type NewWorkspaceBody = Static<typeof NewWorkspaceBody>;
-
-export const NewWorkspaceResponse = Workspace;
-export type NewWorkspaceResponse = Static<typeof NewWorkspaceResponse>;
+import {
+  GetWorkspaceError,
+  GetWorkspaceParams,
+  GetWorkspaceResponse,
+  NewWorkspaceBody,
+  NewWorkspaceResponse,
+  UpdateWorkspaceBody,
+  UpdateWorkspaceError,
+  UpdateWorkspaceParams,
+  UpdateWorkspaceResponse,
+} from "./workspace-routedef";
 
 // Prefix "/workspace"
 const WorkspaceRouteController: FastifyPluginAsync = async (app, opts) => {
+  app.get<{
+    Params: GetWorkspaceParams;
+    Reply: GetWorkspaceResponse | GetWorkspaceError;
+  }>(
+    "/:id",
+    {
+      schema: {
+        description: "Get workspace by id",
+        params: GetWorkspaceParams,
+        response: {
+          200: GetWorkspaceResponse,
+          404: GetWorkspaceError,
+        },
+      },
+    },
+    async (req, res) => {
+      const workspace = await app.prisma.workspace.findUnique({
+        where: { id: req.params.id },
+      });
+
+      if (workspace) return res.code(200).send(workspace);
+
+      return res.code(404).send({ code: 404, message: "Workspace not found" });
+    }
+  );
+
   app.post<{ Body: NewWorkspaceBody; Reply: NewWorkspaceResponse }>(
-    "/new",
+    "/",
     {
       // Limit new workspace for one user to be once every 5 minutes
       preHandler: app.rateLimit({
@@ -30,16 +52,72 @@ const WorkspaceRouteController: FastifyPluginAsync = async (app, opts) => {
         description: "Create a new workspace",
         body: NewWorkspaceBody,
         response: {
-          200: NewWorkspaceResponse,
+          201: NewWorkspaceResponse,
         },
       },
     },
     async (req, res) => {
+      const new_workspace_id = humanId("-");
       const new_workspace = await app.prisma.workspace.create({
-        data: { ...req.body, id: humanId("-"), lastUsed: Date.now() },
+        data: {
+          ...req.body,
+          id: new_workspace_id,
+          lastUsed: Date.now(),
+          timers: {
+            create: {
+              id: new_workspace_id + ":" + humanId("-"),
+              elapsedTime: 0,
+              status: "RESET",
+              time: Date.now(),
+              title: req.body.title + " Default Clock",
+            },
+          },
+        },
       });
 
-      res.send(new_workspace);
+      return res.code(201).send(new_workspace);
+    }
+  );
+
+  app.put<{
+    Params: UpdateWorkspaceParams;
+    Body: UpdateWorkspaceBody;
+    Reply: UpdateWorkspaceResponse | UpdateWorkspaceError;
+  }>(
+    "/:id",
+    {
+      schema: {
+        description: "Update an existing workspace by id",
+        params: UpdateWorkspaceParams,
+        body: UpdateWorkspaceBody,
+        response: {
+          200: UpdateWorkspaceResponse,
+          404: UpdateWorkspaceError,
+        },
+      },
+    },
+    async (req, res) => {
+      const workspace = await app.prisma.workspace.findUnique({
+        where: { id: req.params.id },
+      });
+
+      if (!workspace)
+        return res
+          .code(404)
+          .send({ code: 404, message: "Workspace not found" });
+
+      const updated_workspace = await app.prisma.workspace.update({
+        where: { id: req.params.id },
+        data: {
+          ...req.body,
+        },
+      });
+
+      app.io
+        .in(req.params.id)
+        .emit("workspace:update:" + req.params.id, workspace);
+
+      return res.code(200).send(updated_workspace);
     }
   );
 
