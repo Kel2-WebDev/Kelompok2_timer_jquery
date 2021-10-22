bcfae344b5cd13898f8def31d128b7321769fa47
Fauzan Lubis <fauzanfauh45@gmail.com>
2021-10-22 08:47:21 +0700

    Make frontend as root

--- backend/controller/common-routedef.ts
+++
@@ -1,7 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const ErrorMessage = Type.Object({
-  code: Type.Number(),
-  message: Type.String(),
-});
-export type ErrorMessage = Static<typeof ErrorMessage>;
